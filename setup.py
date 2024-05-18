218c094b1231cd4507ab1b0240980e9e0c06ebd0
Ahmad Ansori Palembani <palembani@gmail.com>
2024-05-18 14:56:02 +0700

    fix: Cython

--- setup.py
+++ setup.py
@@ -38,5 +38,6 @@ setup(
     author_email="erfangplus@gmail.com",
     url="https://github.com/erfanoabdi/gbinder-python",
     license="GPL3",
-    ext_modules=extensions
+    ext_modules=extensions,
+    install_requires=["Cython"],
 )
